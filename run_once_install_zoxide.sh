7ce10115939d95c28710e8c120293b17b1becd9b
Adam Roper <kyushuadamu@protonmail.com>
2024-11-02 02:57:51 +0000

    chore: rearrange fzf install

--- run_once_install_zoxide.sh
+++ run_once_install_zoxide.sh
@@ -4,10 +4,20 @@ set -e
 OS="$(uname -s)"
 case "${OS}" in
     Linux*)
+        # Install zoxide
         if curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh; then
             echo "zoxide installation complete."
         else
             echo "zoxide installation failed."
+            exit 1
+        fi
+
+        # Install fzf
+        if git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf; then
+            ~/.fzf/install
+            echo "fzf installation complete."
+        else
+            echo "fzf installation failed."
             exit 1
         fi
         ;;
